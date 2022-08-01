e54086350a9a99dfec011e90e353bbec86ca6ef5
Norman Munge <normanmunge@gmail.com>
2022-08-01 08:38:29 +0300

    Adds gradient to app screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import StartGame from './screens/StartGame';
 
 export default function App() {
   const { rootScreen } = styles;
   return (
-    <View style={rootScreen}>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={rootScreen}>
       <StartGame />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: '#ddb52f',
   },
 });
