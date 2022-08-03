5d3f4d2630d1194f92c6059a7798e649261790e4
Norman Munge <normanmunge@gmail.com>
2022-08-03 06:47:19 +0300

    Adds logic for setting value and confirming

--- screens/StartGame.tsx
+++ screens/StartGame.tsx
@@ -1,18 +1,54 @@
-import { TextInput, View, StyleSheet } from 'react-native';
+import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/buttons/primary-button';
 import NumberInput from '../components/forms/number-input';
+import { useState } from 'react';
 
 function StartGame() {
   const { inputContainer, buttonsContainer, buttonContainer } = styles;
+
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const enteredValueHandler = (event: any) => {
+    setEnteredValue(event.nativeEvent.text);
+  };
+
+  const resetInputHandler = () => {
+    setEnteredValue('');
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+
+    console.log(
+      'THE CHOSEN NUMBER',
+      chosenNumber,
+      'AND ENTERED VALUE',
+      enteredValue
+    );
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      //show alert...
+      Alert.alert('Invalid Input!', 'Value has to between 0 and 99', [
+        { text: 'Okay', style: 'destructive', onPress: resetInputHandler },
+      ]);
+      return;
+    }
+
+    console.log('Valid number', enteredValue);
+  };
+
   return (
     <View style={inputContainer}>
-      <NumberInput />
+      <NumberInput valueInput={enteredValue} handler={enteredValueHandler} />
       <View style={buttonsContainer}>
         <View style={buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPressHandler={resetInputHandler}>
+            Reset
+          </PrimaryButton>
         </View>
         <View style={buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPressHandler={confirmInputHandler}>
+            Confirm
+          </PrimaryButton>
         </View>
       </View>
     </View>
