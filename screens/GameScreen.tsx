c61cc7356736287aa7a4a42fe9ecd9fbe7c3d88a
Norman Munge <normanmunge@gmail.com>
2022-08-03 07:36:34 +0300

    Adds title compoent

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,28 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Title from '../components/texts/title';
 
 const GameScreen = () => {
-  return <Text>Start Game Screen!</Text>;
+  const { screen } = styles;
+  return (
+    <View style={screen}>
+      <Title>Opponent's Guess</Title>
+      {/* GUESS */}
+      <View>
+        <Text>higher or lower?</Text>
+        {/* + - */}
+      </View>
+      <View>
+        <Text>LOG ROUNDS</Text>
+      </View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
